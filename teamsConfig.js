88bbe85daadb5503ab6a381f0022bfb92f898ecb
multi-DevOps <techinical27.multiicon@gmail.com>
2026-07-20 16:25:43 +0530

    add team details

--- teamsConfig.js
+++ teamsConfig.js
@@ -1,24 +1,48 @@
 // teamsConfig.js
 module.exports = {
-  "Frontend Team": [
-    "Dev 1",
-    "Dev 2",
-    "Dev 3",
-    "Dev 4"
+  "Bonton Team": [
+    "Yash Ghoricha",
+    "Darshi shah",
+    "Tvisha Parekh",
+    "Ankur Kumar",
+    "Hetvi Pedhadiya",
+    "Janvi Kamani",
+    "Jay Moteriya"
   ],
-  "Backend Team": [
-    "Dev 5",
-    "Dev 6",
-    "Dev 7"
+  "Mobile Team": [
+    "Uttam Desai",
+    "Bhavin Sinroja",
+    "Kreena Jotangiya",
+    "Mohit Mandali"
+  ],
+  "MM Team": [
+    "Paresh Desai",
+    "Sahil Rathod",
+    "Parthik Pankhaniya",
+    "Meet-Kacha",
+    "Mudit Bhatt",
+    "Mahesh Dabhi"
+  ],
+  "Forex Team": [
+    "Vivek Kudecha",
+    "Khushi Lachhwani",
+    "kunal pithva",
+    "Nency senjaliya",
+    "Prince solanki"
+  ],
+  "UI/UX Team": [
+    "Heval Patel",
+    "Bhavin Gohil",
+    "Prit Gohel"
   ],
   "DevOps & Infra": [
     "Meet Bundela",
     "Devam Udani",
     "Harsh Bhalodiya"
   ],
-  "QA & Testing": [
-    "Dev 10",
-    "Dev 11"
+  "AI Team ": [
+    "suresh ambechada",
+    "Aditya Vadodariya"
   ]
   // Add all 32 developer names here exactly as spelled in Jira dropdown
 };
